4bfaba07c9121546d9637f5bde05a2b1b1f852d7
radha8272 <radha.infy95@gmail.com>
2025-05-07 11:00:36 +0530

    exploring middleware

--- src/app.js
+++ src/app.js
@@ -4,28 +4,23 @@ const app= express();
 // app.use('/user',[RH1,RH2],RH3,RH4)
 app.use('/user',
     (req,res,next)=>{
-    console.log("handling the route user1 !!")
-  /*   res.send("1st response"); */
+   
+   /* res.send("handling/Route");  */
     next();
+})
+app.get('/user',(req,res,next)=>{
+    console.log("1st response")
+  /*   res.send("handling 1st route") */
+  next();
 },
 (req,res,next)=>{
-    console.log("handling the route user2 !!")
-  /*   res.send("2nd response"); */
-    next();
+    console.log("2nd response");
+    next()
 },
 (req,res,next)=>{
-    console.log("handling the route user3 !!")
-   /*  res.send("3rd response"); */
-    next();
-},
-(req,res,next)=>{
-    console.log("handling the route user4 !!")
-   res.send("4th response"); 
-    next();
-},
-
-),
-
+    res.send("handling 2nd route response");
+}
+)
 app.listen(7777,()=>{
     console.log("server is successfully listening on port 7777...")
 })

7da7badd4a3d071ab796879f59e652c762d414cc
radha8272 <radha.infy95@gmail.com>
2025-05-29 12:19:49 +0530

    deleted a user from a database

--- src/app.js
+++ src/app.js
@@ -17,6 +17,42 @@ app.post('/signup',async(req,res)=>{
     }
    
 })
+app.get("/user",async(req,res)=>{
+    const UserEmail = req.body.emailId;
+try{
+    const users = await User.find({emailId:UserEmail}) 
+    if(users.length===0){
+        res.status(400).send("user not found")
+    }else{
+        res.send(users)
+    }
+ 
+}catch(err){
+    res.status(400).send("something went wrong")
+}
+})
+app.get("/feed",async(req,res)=>{
+    
+try{
+    const users = await User.find({}) 
+    res.send(users)
+    
+ 
+}catch(err){
+    res.status(400).send("something went wrong")
+}
+
+})
+app.delete("/user",async (req,res)=>{
+    const userId= req.body.userId
+    try{
+         /* const user= await User.findByIdAndDelete({_id:userId}) */
+       const user= await User.findByIdAndDelete(userId)  
+        res.send("user deleted successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
 
 
 
